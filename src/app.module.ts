1c2e378e9f7901c208434dab3c29c796ad72ea89
yhsol <loshy244110@gmail.com>
2023-03-27 18:40:24 +0900

    feat: ControllerService in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CalculatorController } from './calculator/calculator.controller';
 import { CalculatorModule } from './calculator/calculator.module';
+import { CalculatorService } from './calculator/calculator.service';
 
 @Module({
   imports: [CalculatorModule],
   controllers: [AppController, CalculatorController],
   providers: [
     AppService,
+    CalculatorService,
     {
       provide: APP_PIPE,
       useClass: ValidationPipe,
